4c31cc4b166f258b98af03106be8da465d6f0caf
Dor Rozenhak <dor851997@gmail.com>
2022-07-03 19:43:02 +0300

    a

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ function BonClick(myId) {
     }
     console.log('bonclick')
 }
+
+var sel1 = document.getElementById('select1');
+
+if(!localStorage.getItem('select1')) {
+    populateStorage();
+} 
+else {
+    setStyles();
+}
+
+function populateStorage() {
+    console.log('populateStorage call');
+    localStorage.setItem('select1',document.getElementById('select1').value);
+    // localStorage.setItem('select1', document.getElementById('select1').value);
+    // setStyles();
+}
+  
+function setStyles() {
+    console.log('setStyles call');
+    var currentSelect1 = localStorage.getItem('select1');
+    // document.getElementById('select1').value = currentSelect1;
+  }
+  
